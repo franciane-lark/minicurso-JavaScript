5fbe189492616bce30543446451bc2f9b2eec288
Franciane Lark <franciane.lark019@gmail.com>
2022-02-18 15:07:39 -0400

    update variáveis

---
+++ index-1.js
@@ -0,0 +1,8 @@
+//como criar uma variável
+const nome = 'Franciane';
+//const não se pude mudar var
+console.log(nome);
+
+let idade = 18;
+//let pode mudar a var
+console.log(idade);
---
+++ index.js
@@ -0,0 +1,3 @@
+//o comando abaixo serve para imprimir uma mensagem na tela
+console.log("Olá, mundo");
+console.log("Olá, Fran!");
