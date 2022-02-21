8a9a918d536f48810e7f72af044228f4cf5fa621
Franciane Lark <franciane.lark019@gmail.com>
2022-02-21 17:00:24 -0400

    update op

---
+++ index-2.js
@@ -0,0 +1,25 @@
+let r1 = 5 + 4;
+let r2 = 5 - 4;
+let r3 = 5 * 4;
+let r4 = 5 ** 4;
+let r5 = (5 + 4) * (3 - 1);
+console.log(r1);
+console.log(r2);
+console.log(r3);
+console.log(r4);
+console.log(r5);
+
+const x = 5;
+const y = 6;
+
+let resultado = x + y;//11
+resultado = resultado + 2;//13
+resultado +=2;//15
+resultado -=2;//13
+resultado *=2;//26
+resultado /=2;//13
+
+
+const resto = 15 % 5;
+
+console.log(resto);
