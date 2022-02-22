53b0150728e945ecbae19e7a26323dcd55f27528
Franciane Lark <franciane.lark019@gmail.com>
2022-02-22 16:04:21 -0400

    update comparacao

---
+++ index-3.js
@@ -0,0 +1,14 @@
+let x = 5;
+let y = 5;
+
+/*
+const resultado = x > y;
+const resultado = x >= y;
+const resultado = x < y;
+const resultado = x <= y;
+const resultado = x == y;
+const resultado = x != y;
+const resultado = x === y;
+*/
+const resultado = !true
+console.log(resultado);
