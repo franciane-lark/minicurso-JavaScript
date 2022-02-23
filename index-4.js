c4b7229a6f687d65332bea6ad8cf5627c50c344c
Franciane Lark <franciane.lark019@gmail.com>
2022-02-23 18:09:58 -0400

    update condicional

---
+++ index-4.js
@@ -0,0 +1,11 @@
+const idade = 15;
+
+//condicionais - if - else
+
+if (idade >= 18) {
+    //só é executado se a condição for verdadeira
+    console.log("Pode seguir, pois é maior de idade");
+}else{
+    //só é executado se a condição for  falsa
+    console.log("Barrado! Este site só é para maiores");
+}
