d482085cd790b1caf1b8c9943252cf5d33b6d8ac
Franciane Lark <franciane.lark019@gmail.com>
2022-03-15 16:41:54 -0400

    update arrays

---
+++ index-9.js
@@ -0,0 +1,15 @@
+/*const ListaDeNomes = ["José", "Maria", "Pedro", "Joana", "André"];
+
+ListaDeNomes [5] = "Johnny"
+ListaDeNomes [1] = "Ana"
+
+console.log(ListaDeNomes);
+*/
+const notas = [];
+//notas [0] = 10;
+notas.push(10);
+notas.push(9);
+notas.push(10);
+notas.pop();
+
+console.log(notas);
